f7814c8435aae1bb3ad0b1a650668125e7dbba09
shubham <shubhamyelekar0@gmail.com>
2024-11-13 20:03:28 +0530

    memor card game done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const arr = ['A','B','C','D','E','F','G','H'];
 const newArr = [...arr, ...arr]
-
+const score = document.getElementById('score')
 // 16 square
 
 const shuffle = (newArr) => {
@@ -15,29 +15,95 @@ const gameBoard =   document.getElementById('gameBoard')
 
 document.addEventListener('DOMContentLoaded', ()=>{
   shuffle(newArr)
-
-  let firstCard
-  let secondCard
+  console.log(newArr)
+  let firstCard = null;
+  let secondCard = null;
 
   let lockBoard = false
   let matchFound = 0
 
-  newArr.forEach((card) => {
+  score.innerText = matchFound
+
+  newArr.forEach((cardValue, index) => {
     const cardBox = document.createElement('div')
     cardBox.classList.add("card")
-    
-    const cardName = document.createElement('span')
-    cardName.innerText = card 
-    
-        cardBox.appendChild(cardName)
-        gameBoard.appendChild(cardBox)
+    cardBox.dataset.value = cardValue; //store the card value in data sttribute
 
-    
+    const cardName = document.createElement('span')
+    cardName.innerText = cardValue
+    cardName.classList.add('hidden')
+    cardBox.appendChild(cardName)
+
+    gameBoard.appendChild(cardBox)
+    cardBox.addEventListener('click', (e)=>{
+      handleCardClick(e, index)
+    })
   })
-  
+
+  function handleCardClick(e,index){
+    cardBox = e.currentTarget
+
+    if (lockBoard){return} // prevent clicking the board
+    if(cardBox === firstCard) {return} // Ignore if same card is clicked
+
+    flipCard(cardBox)
+
+    if(!firstCard){
+      firstCard = cardBox
+    } else {
+      secondCard = cardBox
+      lockBoard = true; // lock until match is complete
+      checkForMatch()
+
+    }
+  }
+  function flipCard(card) {
+    card.classList.add('flipped')
+    card.querySelector('span').classList.remove('hidden')
+  }
+
+  function checkForMatch(){
+    const isMatch = firstCard.dataset.value === secondCard.dataset.value;
+    if(isMatch){
+      matchFound++;
+      score.innerText = matchFound
+      disableCards();
+      resetBoard()
+
+      if(matchFound === arr.length){
+        setTimeout(() => {
+            alert('You Won')
+        }, 500);
+      }
+    }else {
+      unflipCards();
+    }
+
+
+  }
+
+  function unflipCards(){
+    setTimeout(() => {
+      firstCard.classList.remove('flipped')
+      secondCard.classList.remove('flipped')
+      firstCard.querySelector('span').classList.add('hidden');
+      secondCard.querySelector('span').classList.add('hidden');
+      resetBoard()
+    }, 1000);
+  }
+
+  function disableCards(){
+    firstCard.removeEventListener('click', handleCardClick)
+
+    secondCard.removeEventListener('click', handleCardClick)
+
+  }
+
+  function resetBoard(){
+    [firstCard, secondCard] = [null, null]
+    lockBoard = false;
+  }
+
+
 })
 
-function flipCard(card) {
-  if (lockBoard){return}
-  
-}
